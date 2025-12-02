3c1f5a9920a6c9ff57eee02441f19f58ba158014
bleedvowofficial <bleedvowofficial@gmail.com>
2025-12-02 11:13:55 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** NOTE: tailwind.config.js — add Grove design later */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
